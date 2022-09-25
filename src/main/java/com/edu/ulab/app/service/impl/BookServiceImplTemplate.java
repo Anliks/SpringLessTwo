d88adc424e2569f4818c9b64d970dfb375f0a3b6
Anliks <axel_lines@mail.ru>
2022-09-25 19:49:10 +0300

    done

--- src/main/java/com/edu/ulab/app/service/impl/BookServiceImplTemplate.java
+++ src/main/java/com/edu/ulab/app/service/impl/BookServiceImplTemplate.java
@@ -1,17 +1,23 @@
 package com.edu.ulab.app.service.impl;
 
 import com.edu.ulab.app.dto.BookDto;
+import com.edu.ulab.app.entity.Book;
+import com.edu.ulab.app.exception.NotFoundException;
+import com.edu.ulab.app.exception.NotValidDataException;
+import com.edu.ulab.app.mapper.BookMapper;
 import com.edu.ulab.app.service.BookService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -19,9 +25,11 @@ import java.util.Objects;
 public class BookServiceImplTemplate implements BookService {
 
     private final JdbcTemplate jdbcTemplate;
+    BookMapper bookMapper;
 
-    public BookServiceImplTemplate(JdbcTemplate jdbcTemplate) {
+    public BookServiceImplTemplate(JdbcTemplate jdbcTemplate, BookMapper bookMapper) {
         this.jdbcTemplate = jdbcTemplate;
+        this.bookMapper = bookMapper;
     }
 
     @Override
@@ -48,18 +56,70 @@ public class BookServiceImplTemplate implements BookService {
 
     @Override
     public BookDto updateBook(BookDto bookDto) {
-        // реализовать недстающие методы
-        return null;
+        final String UPDATE_SQL = "UPDATE BOOK SET TITLE = ?, AUTHOR = ?, PAGE_COUNT = ? WHERE USER_ID = ?";
+        if (bookDto.getId() == null) {
+            throw new NotFoundException("Id not found ");
+        }
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement ps = con.prepareStatement(UPDATE_SQL);
+                ps.setString(1, bookDto.getTitle());
+                ps.setString(2, bookDto.getAuthor());
+                ps.setLong(3, bookDto.getPageCount());
+                ps.setLong(4, bookDto.getUserId());
+                log.info("Book updated " + bookDto.getId());
+                return ps;
+            }
+        });
+        return bookDto;
     }
 
     @Override
     public BookDto getBookById(Long id) {
-        // реализовать недстающие методы
-        return null;
+        final String SELECT_SQL = "SELECT * FROM BOOK WHERE ID = ?";
+        Book book = jdbcTemplate.queryForObject(SELECT_SQL, new RowMapper<Book>() {
+            @Override
+            public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Book book = new Book();
+                book.setId(rs.getLong("ID"));
+                book.setAuthor(rs.getString("AUTHOR"));
+                book.setTitle(rs.getString("TITLE"));
+                book.setPageCount(rs.getInt("PAGE_COUNT"));
+                book.setUserId(rs.getLong("USER_ID"));
+                return book;
+            }
+        });
+
+        return bookMapper.bookToBookDto(book);
     }
 
     @Override
     public void deleteBookById(Long id) {
-        // реализовать недстающие методы
+        if (id == null) {
+            throw new NotValidDataException("id is null");
+        }
+        final String DELETE_SQL = "DELETE * FROM BOOK WHERE ID = ?";
+        jdbcTemplate.update(DELETE_SQL, id);
     }
+
+    @Override
+    public List<BookDto> listBooksFromUser(Long id) {
+        final String GET_BOOKS_SQL = "SELECT * FROM BOOK WHERE USER_ID = ? ";
+        return jdbcTemplate.query(GET_BOOKS_SQL, (rs, rowNum) -> new BookDto(rs.getLong("id"), rs.getLong("user_id"),
+                rs.getString("title"),
+                rs.getString("author"),
+                rs.getLong("page_count")), id);
+
+    }
+
+    @Override
+    public void deleteBooksByUserId(Long userId) {
+        final String GET_LIST_BOOKS = "DELETE FROM BOOK WHERE USER_ID = ?";
+        if (userId == null) {
+            throw new NotValidDataException("Id is null");
+        }
+        jdbcTemplate.update(GET_LIST_BOOKS, userId);
+    }
+
 }
--- src/main/java/com/edu/ulab/app/service/impl/UserServiceImpl.java
+++ src/main/java/com/edu/ulab/app/service/impl/UserServiceImpl.java
@@ -2,9 +2,12 @@ package com.edu.ulab.app.service.impl;
 
 import com.edu.ulab.app.dto.UserDto;
 import com.edu.ulab.app.entity.Person;
+import com.edu.ulab.app.exception.NotFoundException;
+import com.edu.ulab.app.exception.NotValidDataException;
 import com.edu.ulab.app.mapper.UserMapper;
 import com.edu.ulab.app.repository.UserRepository;
 import com.edu.ulab.app.service.UserService;
+import com.edu.ulab.app.validation.UserValidation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -32,18 +35,38 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto updateUser(UserDto userDto) {
-        // реализовать недстающие методы
-        return null;
+        if (UserValidation.isValid(userDto)) {
+            UserDto findUser = getUserById(userDto.getId());
+            log.info("find user for update {}", findUser);
+            findUser.setAge(userDto.getAge());
+            findUser.setTitle(userDto.getTitle());
+            findUser.setFullName(userDto.getFullName());
+            log.info("user sets column {}", findUser);
+            Person updatedUser = userRepository.save(userMapper.userDtoToPerson(findUser));
+            log.info("user update {}", updatedUser);
+            return userMapper.personToUserDto(updatedUser);
+        } else {
+            throw new NotValidDataException("Not Valid Data");
+        }
     }
 
     @Override
     public UserDto getUserById(Long id) {
-        // реализовать недстающие методы
-        return null;
+        log.info("Got ID for get User {}", id);
+        UserDto userDto = userMapper.personToUserDto(userRepository.findById(id).orElseThrow(() -> new NotFoundException("user ID Not found " + id)));
+        log.info("Got User By ID {}", userDto);
+        return userDto;
     }
 
     @Override
     public void deleteUserById(Long id) {
-        // реализовать недстающие методы
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            log.info("User deleted");
+        } else {
+            throw new NotFoundException("Id not found is base " + id);
+        }
+
     }
+
 }
